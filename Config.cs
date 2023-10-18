5ab66ac98cc2b69ddf83156541ee89a12bc93f75
Samuelelorrieta <samuel.sainz-terronesci@elorrieta-errekamari.com>
2023-10-18 08:20:21 +0200

    Agregar archivos de proyecto.

---
+++ Config.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public class Config
+    {
+        //pongo mi clave de geoapify
+        public static string GeoApifyKey = "6ed2b1c323a5424fb7da02218fd98841";
+    }
+}
